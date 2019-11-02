e942e2f12e888bdf715fed2fa0debcb83dde6f35
Arina Gurinova <arina.gurinova@gmail.com>
2019-11-03 04:52:33 +0600

    Third comment

--- task1.js
+++ task1.js
@@ -1,17 +1,32 @@
 class Product {
     constructor(nameOfProduct, calories) {
-        this.title = nameOfProduct;
-        this.calories = calories;
+        if(calories <= 0 || typeof nameOfProduct === 'undefined' || 
+        typeof calories === 'undefined' || typeof calories === 'string' || 
+        nameOfProduct.length === 0 || typeof nameOfProduct === 'number') console.log('Enter the correct product name or the correct number of calories');
+        else {
+            this.title = nameOfProduct;
+            this.calories = calories;
+        }
     }
 }
 
 class Dish {
     constructor(nameOfDish) {
-        this.title = nameOfDish;
-        this.products = [];
+        if(typeof nameOfDish === 'undefined' || typeof nameOfDish === 'number' || 
+        nameOfDish.length === 0) console.log('Enter the correct dish name');
+        else {
+            this.title = nameOfDish;
+            this.products = [];
+        }
     }
     addProduct(ingredient, quantity) {
-        this.products.push([ingredient, quantity]);
+        if(quantity <= 0 || typeof ingredient === 'undefined' || typeof quantity === 'undefined' || 
+        ingredient.length === 0 || typeof ingredient === 'number' ||  typeof quantity === 'string') {
+            console.log('Enter the correct ingredient name or the correct number of quantity');
+        }
+        else {
+            this.products.push([ingredient, quantity]);
+        }
     }
     getCalories() {
         let calories = 0;
@@ -31,7 +46,8 @@ class CaloriesCalculator {
     }
     
     addDish(nameOfDish) {
-        this.nameOfDish.push(nameOfDish);
+        if(typeof nameOfDish === 'undefined' || typeof nameOfDish === "number") console.log('Enter the correct dish name');
+        else this.nameOfDish.push(nameOfDish);
     }
     
     getTotalCalories() {
@@ -63,14 +79,33 @@ const rice = new Product('Рис', 130);
 const onion = new Product('Лук', 40);
 const carrot = new Product('Морковь', 41);
 
+const meat1 = new Product('', -158);
+const meat2 = new Product(138);
+const meat3 = new Product();
+const meat4 = new Product(158, 'Рис');
+
 const plov = new Dish('Плов');
+
+const plovTest1 = new Dish('');
+const plovTest2 = new Dish();
+const plovTest3 = new Dish(-15);
+
 plov.addProduct(meat, 100);
 plov.addProduct(rice, 150);
 plov.addProduct(onion, 25);
 plov.addProduct(carrot, 25);
 
+plov.addProduct();
+plov.addProduct('', -20);
+plov.addProduct(meat, -20);
+plov.addProduct(20);
+
+
 const calculator = new CaloriesCalculator();
 calculator.addDish(plov);
+
+calculator.addDish();
+calculator.addDish(-20);
 
 const calories = calculator.getTotalCalories();
 console.log(calories);
